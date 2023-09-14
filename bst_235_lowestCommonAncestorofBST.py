ae4294e54fc1caf280755b5dd8e1970a539cb765
Amisha Himanshu Somaiya <amishahs@uw.edu>
2023-09-14 12:01:43 -0700

    Update bst_235_lowestCommonAncestorofBST.py

--- bst_235_lowestCommonAncestorofBST.py
+++ bst_235_lowestCommonAncestorofBST.py
@@ -17,7 +17,20 @@ and other node is descendent of it
 so in this specific eg., since p=2 is in left subtree and q=8 is in right subtree, there is no common 
 node other than the root.   
 
-Time Complexity: 
+another eg: 7 and 9 : both are > root. so dont need to search in left subtree, only search in 
+right subtree. 
+
+so cases :
+case 1 : 
+if 1 in left subtree and other in right subtree : root is LCA
+case 2 :
+if both are in 1 subtree, do not search in other subtree
+case 3 : 
+if one of the nodes is root, then LCA is root
+
+Time Complexity: o(logn) since only visiting 1 node per level = also equal to the height of the
+tree and do not need to visit every single node in the tree so not o(n)
+space complexity = o(1) since no extra space used 
 
 """
 
@@ -30,15 +43,17 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        cur = root
-        while cur:
-            if p.val > cur.val and q.val > cur.val:
-                cur = cur.right
-            elif p.val < cur.val and q.val < cur.val:
+        cur = root  #begin at root
+        while cur:  #continue till result achieved which is guaranteed to exist in the input tree
+            # so the while condition is while cur
+            if p.val > cur.val and q.val > cur.val:  #case1 : both p and q are > root or current value we r visiting:
+                # so go down the right subtree
+                cur = cur.right  #update current pointer accordingly
+            elif p.val < cur.val and q.val < cur.val:  #opposite case
                 cur = cur.left
-            else:
+            else:   #last case : split occurs or we found p or q as root/current so return current  
                 return cur
-            
+            # guaranteed to execute at some point so no return outside of loop needed 
 '''
 testing :
 Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
