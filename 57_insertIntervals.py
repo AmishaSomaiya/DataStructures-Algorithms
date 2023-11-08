778c52b856c3a2c0a34c7e697d56665a8f2bc5b8
Amisha Himanshu Somaiya <amishahs@uw.edu>
2023-11-07 18:00:15 -0800

    Update 57_insertIntervals.py

--- 57_insertIntervals.py
+++ 57_insertIntervals.py
@@ -61,6 +61,22 @@ repeat till ints remaining
 Time Complexity: since already sorted so only go thru once
 so o(n): linear and space : o(n) for result array 
 
+
+ res = []
+ for i in range(len(intervals)):
+         if newInterval[1] < intervals[i][0]:
+                 res.append(newInterval)
+                 return res + intervals[i:]
+         elif newInterval[0] > intervals[i][1]:
+                 res.append(intervals[i])
+         else:
+              newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1]),
+                ]
+        res.append(newInterval)
+        return res
+
 """
 
 from typing import List
