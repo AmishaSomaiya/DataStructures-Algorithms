8d9aece9e4ef633fbd7720d7387d63d25cbea28a
Amisha Himanshu Somaiya <amishahs@uw.edu>
2023-11-27 09:15:08 -0800

    Update binarySearch_704_binarySearch.py

--- binarySearch_704_binarySearch.py
+++ binarySearch_704_binarySearch.py
@@ -16,26 +16,49 @@ Logic : use iterative approach and avoid overflow while calculating mid
 Time Complexity: o(logn) since array divided by 2 at each step
 
 """
-
 class Solution:
-    def search(self, nums, target: int) -> int:
-        # search space is entire array
+    def search(self, nums: List[int], target: int) -> int:
+
+        # simple binary search
+
         l=0
         r=len(nums)-1
 
-        # iterative 
-        while(l<=r):
-            mid = l + ((r-l)//2) #to avoid overflow
-
-            if nums[mid] == target : #target found 
+ 
+        while l<=r:  #remember this is <= else l at last position will not work
+            mid = (l+r)//2
+            if nums[mid]==target:
                 return mid
-            elif nums[mid] > target : #search left
+            elif target > nums[mid]:
+                l=mid+1
+            else : 
                 r = mid-1
-            elif nums[mid] < target : #search right
-                l = mid + 1
 
-        # if target not found
+
         return -1
+
+obj = Solution()
+print(obj.search([2,5],5))
+
+# class Solution:  (with detailed comments)
+#     def search(self, nums, target: int) -> int:
+#         # search space is entire array
+#         l=0
+#         r=len(nums)-1
+
+#         # iterative 
+#         while(l<=r):
+#             mid = l + ((r-l)//2) #to avoid overflow
+
+#             if nums[mid] == target : #target found 
+#                 return mid
+#             elif nums[mid] > target : #search left
+#                 r = mid-1
+#             elif nums[mid] < target : #search right
+#                 l = mid + 1
+
+#         # if target not found
+#         return -1
     
 
 
